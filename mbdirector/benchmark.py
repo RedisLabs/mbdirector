90a89ab1c14d1ccfd8728cfce99117de9dfac34c
filipecosta90 <filipecosta.90@gmail.com>
2020-02-27 14:17:48 +0000

    [fix] fix argument must be str, not bytes Error on write_file('mb.stderr', _stderr)

--- mbdirector/benchmark.py
+++ mbdirector/benchmark.py
@@ -54,7 +54,10 @@ class Benchmark(object):
         return cls(config, **json)
 
     def write_file(self, name, data):
-        with open(os.path.join(self.config.results_dir, name), 'w') as outfile:
+        mode = 'w'
+        if type(data)==bytes:
+            mode = 'wb'
+        with open(os.path.join(self.config.results_dir, name), mode) as outfile:
             outfile.write(data)
 
     def run(self):
